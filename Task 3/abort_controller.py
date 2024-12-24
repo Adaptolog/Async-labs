040d4383070e1fd80e1eb80bc7f1c6432b4062e6
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-12-24 17:26:51 +0200

    Implement async order processing with random delay
    -Реалізовано базову асинхронну функцію process_order, яка імітує обробку замовлення з випадковим часом затримки.

--- Task 3/abort_controller.py
+++ Task 3/abort_controller.py
@@ -1,1 +1,15 @@
+import asyncio
+import random
+import time
 
+async def process_order(order: str, min_time: float = 1.5) -> str:
+    """
+    Імітує асинхронну обробку замовлення.
+    """
+    preparation_time = random.uniform(0.5, 3)
+    await asyncio.sleep(preparation_time)
+    elapsed_time = max(preparation_time, min_time)
+
+    if random.random() < 0.2:  # 20% шанс на помилку
+        raise Exception(f"Помилка обробки замовлення: {order}")
+    return f"Замовлення {order} готове за {elapsed_time:.2f} секунд."
