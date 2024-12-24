c0c76d4e543ca2133f21648321a5e08e575932a5
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-12-24 17:10:45 +0200

    Add asynchronous order processing function
    -Додано асинхронну функцію process_order_async, яка імітує обробку замовлення.
    -Використовується asyncio.sleep для імітації асинхронної затримки, передбачено обробку помилок.

--- Task 2/async_await.py
+++ Task 2/async_await.py
@@ -1,1 +1,16 @@
+import asyncio
+import random
+from typing import List, Optional, Tuple
 
+
+async def process_order_async(order: str, min_time: float = 1.5) -> str:
+    """
+    Асинхронна функція для обробки замовлення.
+    """
+    preparation_time = random.uniform(0.5, 3)
+    await asyncio.sleep(preparation_time)
+    elapsed_time = max(preparation_time, min_time)
+
+    if random.random() < 0.2:  # 20% шанс на помилку
+        raise Exception(f"Помилка обробки замовлення: {order}")
+    return f"Замовлення {order} готове за {elapsed_time:.2f} секунд."
