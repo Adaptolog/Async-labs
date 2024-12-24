314b2ece574c3e1b5bded154fecbb1890793bc98
Adaptolog <143221626+Adaptolog@users.noreply.github.com>
2024-12-24 18:44:54 +0200

    Added the EventEmitter class for handling reactive communication.
    -Додано клас EventEmitter для реактивної взаємодії між сутностями. 
    -Реалізовано методи для додавання спостерігачів (subscribe) і надсилання повідомлень (emit).

--- Task 5/reactive_system.py
+++ Task 5/reactive_system.py
@@ -1,1 +1,20 @@
+import logging
 
+logging.basicConfig(level=logging.INFO)
+
+class EventEmitter:
+    def __init__(self):
+        self.subscribers = []
+
+    def subscribe(self, callback):
+        """
+        Додає нового спостерігача.
+        """
+        self.subscribers.append(callback)
+
+    def emit(self, message):
+        """
+        Надсилає повідомлення всім спостерігачам.
+        """
+        for subscriber in self.subscribers:
+            subscriber(message)
